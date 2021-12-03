49098866ca4f19d8d198909a5c57ef7ba39ad352
Richard <richardmenning@googlemail.com>
2021-12-03 21:46:23 +0100

    Initial Commit

---
+++ playground_nova/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  playground_nova
+//
+//  Created by Richard Menning on 03.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ playground_nova/playground_novaApp.swift
@@ -0,0 +1,17 @@
+//
+//  playground_novaApp.swift
+//  playground_nova
+//
+//  Created by Richard Menning on 03.12.21.
+//
+
+import SwiftUI
+
+@main
+struct playground_novaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
